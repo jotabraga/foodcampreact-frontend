71bc8eee112c9bd9e8081b735ff5137da800cbc1
jotabraga <jpmbraga.feg@gmail.com>
2021-05-06 12:41:03 -0300

    correção de bug de componentização

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,5 +1,3 @@
-
-
 export default function Food(props){
 
     const {image,title,description,price} = props;
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,21 +6,21 @@ import Desserts from "./Desserts";
 export default function Menu(){
 
     const dishesOptions = [
-        {titulo: "Frango Yin Yang",image: "img/frango_yin_yang.png", description: "Um pouco de batata, um pouco de salada",price: "14,90"},
-        {titulo: "Bife à cavalo",image: "img/bifeacavalo.jpg", description: "Bife mignon com ovo frito",price: "17,90"},
-        {titulo: "Vegetariano",image: "img/vegetariano.jpg", description: "Salada de alface tomate e cenoura" ,price: "16,90"}
+        {title: "Frango Yin Yang",image: "img/frango_yin_yang.png", description: "Um pouco de batata, um pouco de salada",price: "14,90"},
+        {title: "Bife à cavalo",image: "img/bifeacavalo.jpg", description: "Bife mignon com ovo frito",price: "17,90"},
+        {title: "Vegetariano",image: "img/vegetariano.jpg", description: "Salada de alface tomate e cenoura" ,price: "16,90"}
     ]
 
     const drinksOptions = [
-        {titulo: "Coquinha gelada",image: "img/coquinha_gelada.png", description: "Coquinha trincando c/ limão",price: "14,90"},
-        {titulo: "Chopp gelado",image: "img/chopp.jpg", description: "Chopp gelado Heineken",price: "14,90"},
-        {titulo: "Suco natural",image: "img/suco.jpg", description: "Suco natural feito na hora",price: "14,90"}
+        {title: "Coquinha gelada",image: "img/coquinha_gelada.png", description: "Coquinha trincando c/ limão",price: "14,90"},
+        {title: "Chopp gelado",image: "img/chopp.jpg", description: "Chopp gelado Heineken",price: "14,90"},
+        {title: "Suco natural",image: "img/suco.jpg", description: "Suco natural feito na hora",price: "14,90"}
     ]
 
     const dessertsOptions = [
-        {titulo: "Pudim",image: "img/pudim.png", description: "Pudim caseiro de leite moça",price: "14,90"},
-        {titulo: "Sorvete",image: "img/sorvete.jpg", description: "Sorvete gelato com duas bolas",price: "14,90"},
-        {titulo: "Torta",image: "img/torta.jpg", description: "Torta de limão ou morango",price: "14,90"},
+        {title: "Pudim",image: "img/pudim.png", description: "Pudim caseiro de leite moça",price: "14,90"},
+        {title: "Sorvete",image: "img/sorvete.jpg", description: "Sorvete gelato com duas bolas",price: "14,90"},
+        {title: "Torta",image: "img/torta.jpg", description: "Torta de limão ou morango",price: "14,90"},
     ]
 
   
@@ -31,7 +31,7 @@ export default function Menu(){
             <div class="titulo">Primeiro, seu prato</div>
             <div class="opcoes pratos">
 
-            {dishesOptions.map(item => <Dishes title={item.titulo} image={item.image} description={item.description} price={item.price}/>)}
+            {dishesOptions.map(item => <Dishes item={item}/>)}
 
             </div>
           </div>
@@ -40,7 +40,7 @@ export default function Menu(){
             <div class="titulo">Agora, sua bebida</div>
             <div class="opcoes bebidas">
 
-            {drinksOptions.map(item => <Drinks title={item.titulo} image={item.image} description={item.description} price={item.price}/>)}
+            {drinksOptions.map(item => <Drinks item={item}/>)}
 
             </div>
 
@@ -51,7 +51,7 @@ export default function Menu(){
 
             <div class="opcoes sobremesas">
 
-            {dessertsOptions.map(item => <Desserts title={item.titulo} image={item.image} description={item.description} price={item.price}/>)}
+            {dessertsOptions.map(item => <Desserts item={item}/>)}
 
 
             </div>
