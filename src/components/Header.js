f6e5822205d098386eedefc1b3f91e65cb7c1efc
ronald <ronaldjunior2@hotmail.com>
2021-05-11 19:01:33 -0300

    preparando review

--- src/components/Header.js
+++
@@ -1,8 +0,0 @@
-export default function Header(){
-    return (
-        <div className="header">
-            <div className="titulo">FoodCamp</div>
-            <div className="subtitulo">Sua comida em 6 minutos</div>
-        </div>
-    );
-}
